f56069b49906cea9c1659ea514965372adfae604
ralphvw <ralphvwilliams@icloud.com>
2025-01-28 19:30:29 +0000

    library name

--- build.zig
+++ build.zig
@@ -13,7 +13,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const lib = b.addStaticLibrary(.{
-        .name = "Rediz",
+        .name = "rediz",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
         .root_source_file = b.path("src/redis.zig"),
